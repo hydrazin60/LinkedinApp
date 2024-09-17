50cd0304285560f10576855cf81c2dfb7d471cef
hydrazin60 <sureshkhdk45@gmail.com>
2024-09-17 23:09:24 +0545

    Login UI/Screen is design

---
+++ app/(authenticate)/login.jsx
@@ -0,0 +1,137 @@
+import {
+  View,
+  SafeAreaView,
+  Image,
+  Text,
+  KeyboardAvoidingView,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
+import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Link } from "expo-router";
+
+const login = () => {
+  const [PasswordSee, setPasswordSee] = React.useState(true);
+  return (
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
+    >
+      <View>
+        <Image
+          style={{ width: 150, height: 100, resizeMode: "contain" }}
+          source={{
+            uri: "https://www.freepnglogos.com/uploads/linkedin-logo-transparent-png-25.png",
+          }}
+        />
+      </View>
+      <ScrollView>
+        <KeyboardAvoidingView behavior="padding">
+          <View style={{ alignItems: "center" }}>
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: "bold",
+                color: "#0077b5",
+              }}
+            >
+              Log in to your account
+            </Text>
+          </View>
+          <View style={{ marginTop: 60 }}>
+            <View style={styles.inputSectionContainner}>
+              <TextInput
+                placeholder="Enter your email"
+                placeholderTextColor={"gray"}
+                style={{ paddingHorizontal: 10 }}
+              />
+            </View>
+          </View>
+          <View
+            style={[
+              styles.inputSectionContainner,
+              { flexDirection: "row", alignItems: "center" },
+            ]}
+          >
+            <TextInput
+              placeholder="Enter your password"
+              placeholderTextColor={"gray"}
+              secureTextEntry={PasswordSee}
+              style={{ flex: 1, paddingHorizontal: 10 }}
+            />
+            <TouchableOpacity onPress={() => setPasswordSee(!PasswordSee)}>
+              <Ionicons
+                name={PasswordSee ? "eye-off" : "eye"}
+                size={24}
+                color="black"
+              />
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              marginTop: 12,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text> Keep me Logged in</Text>
+            <Text style={{ color: "#007fff", fontWeight: "500" }}>
+              Forgot Password
+            </Text>
+          </View>
+          <View style={{ marginTop: 80 }}>
+            <Pressable
+              style={{
+                width: 300,
+                backgroundColor: "#0077b5",
+                borderRadius: 5,
+                marginLeft: "auto",
+                marginRight: "auto",
+                padding: 15,
+              }}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  textAlign: "center",
+                  fontSize: 16,
+                  fontWeight: "600",
+                }}
+              >
+                Login
+              </Text>
+            </Pressable>
+            <Pressable style={{ marginTop: 15 }}>
+              <Text
+                style={{ textAlign: "center", fontSize: 16, color: "gray" }}
+              >
+                Don't have an account?{" "}
+                <Link href="/Register" style={{ color: "#0077b5" }}>
+                  Register
+                </Link>{" "}
+              </Text>
+            </Pressable>
+          </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+export default login;
+
+const styles = StyleSheet.create({
+  inputSectionContainner: {
+    padding: 10,
+    height: 50,
+    width: 300,
+    borderColor: "#0077b5",
+    backgroundColor: "#f2f2f2",
+    marginVertical: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+});
--- server/models/post.models.js
+++ server/models/post.models.js
@@ -1,36 +1,39 @@
 import mongoose from "mongoose";
 
-const PostSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    default: function () {
-      return `Post created on: ${new Date().toLocaleString()}`;
+const PostSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      default: function () {
+        return `Post created on: ${new Date().toLocaleString()}`;
+      },
     },
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  authorID: {  
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  likes: [
-    {
+    image: {
+      type: String,
+      required: true,
+    },
+    authorID: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
-  ],
-  comments: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Comment",
-    },
-  ],
-}, {
-  timestamps: true,  
-});
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Post = mongoose.model("Post", PostSchema);
 export default Post;
